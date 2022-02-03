6a710430d5eb1fe25b568a3c4b22f088796b5331
stephc23 <88057766+stephc23@users.noreply.github.com>
2022-02-03 05:48:26 -0500

    Update addModalHTML and showModal in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-const randomUserURL = 'https://randomuser.me/api/?results=12&nat=us&inc=name,location,email,dob,cell,picture&seed=awesome';
+const randomUserURL = 'https://randomuser.me/api/?results=12&nat=us&inc=name,location,email,dob,cell,picture';
 const body = document.querySelector('body');
 const galleryDiv = document.querySelector('#gallery');
 const cardDivs = galleryDiv.children;
@@ -10,13 +10,15 @@ const cardDivs = galleryDiv.children;
 /**
  * Request data and, if the response is successful, parse the data as JSON.
  * @param {string} url - The API endpoint.
- * @returns {object} A promise that resolves to the parsed JSON.
+ * @returns {object} A promise that resolves to an array of objects, each representing an employee.
  */
-async function getJSON(url) {
+async function getEmployees(url) {
     try {
         const response = await fetch(url);
         if (response.ok) {
-            return await response.json();
+            const json = await response.json();
+            const employees = json.results;
+            return employees;
         } else {
             throw `Fetch unsuccessful: ${response.status}`;
         }
@@ -24,10 +26,6 @@ async function getJSON(url) {
         console.log(error);
     }
 }
-
-// ------------------------------------------------------------------------
-// Functions to get and display random employees
-// ------------------------------------------------------------------------
 
 /**
  * Generate HTML for the gallery using data from the API.
@@ -47,17 +45,53 @@ function addGalleryHTML(employees) {
         </div>
     `).join('');
     galleryDiv.insertAdjacentHTML('beforeend', galleryHTML);
+}   
+
+/**
+ * Generates HTML for the modal window.
+ * @param {array} employees - The array of objects from the API, each representing an employee.
+ * @param {number} index - The index of the card div the user clicks on, which is also the index of that employee object in the array.
+ * @returns {object} A promise that resolves to the modal HTML.
+ */
+ function addModalHTML(employees) { 
+    employees.forEach(employee => {
+        const dob = employee.dob.date;
+        const dobFormatted = `${dob.slice(5, 7)}/${dob.slice(8, 10)}/${dob.slice(0, 4)}`;
+        const cellFormatted = employee.cell.replace('-', ' ');
+
+        const script = body.lastElementChild;
+        const modalDiv = document.createElement('div');
+        modalDiv.className = 'modal-container';
+        modalDiv.innerHTML = `
+            <div class="modal">
+                <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                <div class="modal-info-container">
+                    <img class="modal-img" src=${employee.picture.large} alt="profile picture">
+                    <h3 id="name" class="modal-name cap">${employee.name.first} ${employee.name.last}</h3>
+                    <p class="modal-text">${employee.email}</p>
+                    <p class="modal-text cap">${employee.location.city}</p>
+                    <hr>
+                    <p class="modal-text">${cellFormatted}</p>
+                    <p class="modal-text">${employee.location.street.number} ${employee.location.street.name}, ${employee.location.city}, ${employee.location.state} ${employee.location.postcode}</p>
+                    <p class="modal-text">Birthday: ${dobFormatted}</p>
+                </div>
+            </div>
+        `;
+        body.insertBefore(modalDiv, script);
+        modalDiv.style.display = 'none';
+    });
+ }
+
+async function generateHTML() {
+    const employees = await getEmployees(randomUserURL);
+    addGalleryHTML(employees);
+    addModalHTML(employees);
 }
 
-getJSON(randomUserURL)
-    .then(json => {
-        const employees = json.results;
-        addGalleryHTML(employees);
-    });
+generateHTML();
 
-// ------------------------------------------------------------------------
-// Functions to create a modal window
-// ------------------------------------------------------------------------
+
+
 
 /**
  * Get the index of the employee the user clicks on by finding the div stored in `cards` that matches the clicked div.
@@ -84,57 +118,19 @@ function getEmployeeIndex(event) {
 }
 
 /**
- * Generates HTML for the modal window.
- * @param {number} index - The index of the card div the user clicks on, which is also the index of that employee object in the array.
- * @returns {object} A promise that resolves to the modal HTML.
- */
-async function generateModalHTML(index) {
-    const json = await getJSON(randomUserURL);
-    const employees = json.results;
-    const employee = employees[index];
-    const dob = employee.dob.date;
-    const dobFormatted = `${dob.slice(5, 7)}/${dob.slice(8, 10)}/${dob.slice(0, 4)}`;
-    const cellFormatted = employee.cell.replace('-', ' ');
-    const modalHTML = `
-        <div class="modal-container">
-            <div class="modal">
-                <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-                <div class="modal-info-container">
-                    <img class="modal-img" src=${employee.picture.large} alt="profile picture">
-                    <h3 id="name" class="modal-name cap">${employee.name.first} ${employee.name.last}</h3>
-                    <p class="modal-text">${employee.email}</p>
-                    <p class="modal-text cap">${employee.location.city}</p>
-                    <hr>
-                    <p class="modal-text">${cellFormatted}</p>
-                    <p class="modal-text">${employee.location.street.number} ${employee.location.street.name}, ${employee.location.city}, ${employee.location.state} ${employee.location.postcode}</p>
-                    <p class="modal-text">Birthday: ${dobFormatted}</p>
-                </div>
-            </div>
-        </div>
-    `;
-    return modalHTML;
-}
-
-/**
- * Add event listener to the modal close button.
- */
-function activateModalClose() {
-    const closeButton = body.querySelector('#modal-close-btn');
-    const modalContainer = body.querySelector('.modal-container');
-    closeButton.addEventListener('click', () => {
-        body.removeChild(modalContainer);
-    });
-}
-
-/**
  * Display the modal by calling `getEmployeeIndex` and `generateModalHTML`; activate modal close button after inserting the modal HTML.
  * @param {object} event - The event object from the event handler.
  */
-async function showModal(event) {
+function showModal(event) {
     const index = getEmployeeIndex(event);
-    const modalHTML = await generateModalHTML(index);
-    body.insertAdjacentHTML('beforeend', modalHTML);
-    activateModalClose();
+    const modalDivs = document.querySelectorAll('.modal-container');
+    const activeModalDiv = modalDivs[index];
+    const closeButton = activeModalDiv.firstElementChild.firstElementChild;
+    
+    activeModalDiv.style.display = 'block'; 
+    closeButton.addEventListener('click', () => {
+        activeModalDiv.style.display = 'none';
+    });
 }
 
 // ------------------------------------------------------------------------
@@ -146,4 +142,3 @@ galleryDiv.addEventListener('click', event => {
         showModal(event);
     }
 });
-
